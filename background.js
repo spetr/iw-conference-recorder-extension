b8f73e9b3588d7bc26be21be21a97798a13659c9
Stanislav Petr <petr@digitaldata.cz>
2021-07-31 22:45:29 +0200

    Code cleanup

--- background.js
+++ background.js
@@ -1,6 +1,11 @@
 'use strict';
 
+const audioBitsPerSecond = 128000;
+const videoBitsPerSecond = 2610000;
+const videoCodec = 'VP8'; // VP8, VP9, H264
+
 let runtimePort;
+let isRecording = false;
 
 // Listen external commands (startRecord / stopRecord)
 chrome.runtime.onConnect.addListener(function (port) {
@@ -21,15 +26,11 @@ chrome.runtime.onConnect.addListener(function (port) {
 
 // Listen tab events
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    if (tab.active === false) {
+    if (isRecording === true || tab.active === false) {
         return
     }
-    console.log("changeInfo", changeInfo);
     switch (changeInfo.status) {
         case 'complete':
-            if (isRecording) {
-                return;
-            }
             var pattern = /^((http|https):\/\/)/;
             if (!pattern.test(tab.url)) {
                 return;
@@ -57,19 +58,24 @@ function startScreenRecording() {
                 minHeight: 9,
                 maxWidth: 1920,
                 maxHeight: 1080,
-                maxFrameRate: 24,
+                maxFrameRate: 24
             }
         }
     };
     chrome.tabCapture.capture(constraints, function (stream) {
-        var newStream = new MediaStream();
+        if (chrome.runtime.lastError) {
+            console.log('Error while capturing tab.');
+            console.log(chrome.runtime.lastError.message);
+            return;
+        }
+        var tabCaptureStream = new MediaStream();
         try {
             stream.getTracks().forEach(function (track) {
-                newStream.addTrack(track);
+                tabCaptureStream.addTrack(track);
             });
         } catch (e) {
-            alert("Please run chrome with:\n--whitelisted-extension-id=" + chrome.runtime.id)
-            return true
+            alert("Please run chrome with:\n--whitelisted-extension-id=" + chrome.runtime.id);
+            return;
         }
 
         var options = {
@@ -79,7 +85,6 @@ function startScreenRecording() {
             audioBitsPerSecond: audioBitsPerSecond,
             videoBitsPerSecond: videoBitsPerSecond,
         };
-
         switch (videoCodec) {
             case 'VP8':
                 options.mimeType = 'video/webm; codecs="vp8, opus"';
@@ -88,98 +93,35 @@ function startScreenRecording() {
                 options.mimeType = 'video/webm; codecs="vp9, opus"';
                 break;
             case 'H264':
-                options.mimeType = 'video/x-matroska; codecs="h264"';
-                break;
-            case 'AVC1':
-                options.mimeType = 'video/x-matroska; codecs="avc1"';
+                options.mimeType = 'video/webm; codecs="h264, opus"';
                 break;
             default:
                 console.log("Unknown video codec");
                 return;
         }
 
-        recorder = new MediaStreamRecorder(newStream, options);
-        recorder.streams = [newStream];
-        recorder.record();
+        var mediaRecorder = new MediaRecorder(stream, options);
+        mediaRecorder.ondataavailable = function (event) {
+            if (event.data.size > 0) {
+                var xhr = new XMLHttpRequest();
+                xhr.open('POST', 'http://127.0.0.1:80/record', true);
+                xhr.setRequestHeader('Content-Type', "video/webm");
+                xhr.setRequestHeader('X-ID', "test.webm")
+                xhr.send(event.data);
+                xhr.onreadystatechange = function () { // Call a function when the state changes.
+                    if (this.readyState === XMLHttpRequest.DONE) {
+                        if (this.status === 200) {
+                            console.log("Succesfuly send chunk to API.");
+                            return
+                        }
+                        console.log("Error while sending chunk to API");
+                        console.log(this);
+                    }
+                }
+            }
+        };
+
+        mediaRecorder.start(5000);
         isRecording = true;
-
-        addStreamStopListener(recorder.streams[0], function () {
-            stopScreenRecording();
-        });
-
-        initialTime = Date.now()
-        timer = setInterval(checkTime, 100);
     });
 };
-
-// Stop recording
-function stopScreenRecording() {
-    if (!recorder || !isRecording) return;
-    if (timer) {
-        clearTimeout(timer);
-    }
-    setBadgeText('');
-    isRecording = false;
-    recorder.stop(function onStopRecording(blob) {
-        var mimeType = 'video/webm';
-        var fileExtension = 'webm'
-        if (recorder && recorder.streams) {
-            recorder.streams.forEach(function (stream, idx) {
-                stream.getTracks().forEach(function (track) {
-                    track.stop();
-                });
-                if (idx == 0 && typeof stream.onended === 'function') {
-                    stream.onended();
-                }
-            });
-            recorder.streams = null;
-        }
-        isRecording = false;
-        setBadgeText('');
-
-        var xhr = new XMLHttpRequest();
-        console.log(xhr)
-        xhr.open('POST', 'http://127.0.0.1:80/record', true);
-        xhr.setRequestHeader('Content-Type', mimeType);
-        xhr.setRequestHeader('X-ID', getFileName(fileExtension))
-        xhr.send(recorder.blob);
-        xhr.onreadystatechange = function () { // Call a function when the state changes.
-            if (this.readyState === XMLHttpRequest.DONE)
-                if (this.status === 200) {
-                    console.log("Got response 200 from API!");
-                    return
-                }
-            console.log("Error")
-        }
-    });
-}
-
-function convertTime(miliseconds) {
-    var totalSeconds = Math.floor(miliseconds / 1000);
-    var minutes = Math.floor(totalSeconds / 60);
-    var seconds = totalSeconds - minutes * 60;
-    minutes += '';
-    seconds += '';
-    if (seconds.length === 1) {
-        seconds = '0' + seconds;
-    }
-    return minutes + ':' + seconds;
-}
-
-var initialTime, timer;
-
-function checkTime() {
-    if (!initialTime || !isRecording) return;
-    var timeDifference = Date.now() - initialTime;
-    var formatted = convertTime(timeDifference);
-    setBadgeText(formatted);
-}
-
-function setBadgeText(text) {
-    chrome.browserAction.setBadgeBackgroundColor({
-        color: [255, 0, 0, 255]
-    });
-    chrome.browserAction.setBadgeText({
-        text: text + ''
-    });
-}

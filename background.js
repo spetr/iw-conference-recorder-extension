4d82eaa61f3a33dc177a7a04337e43fe3028f201
Stanislav Petr <petr@digitaldata.cz>
2021-08-02 13:33:55 +0200

    Debug messagess + fixies

--- background.js
+++ background.js
@@ -5,35 +5,22 @@ const videoBitsPerSecond = 2610000;
 const videoCodec = 'VP8'; // VP8, VP9, H264
 const apiURL = 'http://127.0.0.1:80/record';
 
-let runtimePort;
 let isRecording = false;
-
-// Listen external commands (startRecord / stopRecord)
-chrome.runtime.onConnect.addListener(function (port) {
-    runtimePort = port;
-    runtimePort.onMessage.addListener(function (message, s, senderResponse) {
-        if (!message) {
-            return;
-        }
-        switch (message.action) {
-            case "stopRecord":
-                stopScreenRecording();
-                break;
-            default:
-                console.log(`unknown action ${request.action}`);
-        }
-    })
-});
 
 // Listen tab events
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    if (isRecording === true || tab.active === false) {
-        return
+    if (tab.active === false) {
+        return;
     }
     switch (changeInfo.status) {
         case 'complete':
             var pattern = /^((http|https):\/\/)/;
             if (!pattern.test(tab.url)) {
+                console.log('URL of active tab is not starting with http:// or https://')
+                return;
+            }
+            if (isRecording === true) {
+                console.log('Recording is running');
                 return;
             }
             startScreenRecording();
@@ -44,6 +31,7 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
 
 // Start recording (current tab)
 function startScreenRecording() {
+    console.log('Starting tab recording.')
     var constraints = {
         audio: true,
         video: true,
@@ -67,15 +55,18 @@ function startScreenRecording() {
         if (chrome.runtime.lastError) {
             console.log('Error while capturing tab.');
             console.log(chrome.runtime.lastError.message);
-            alert("Please run chrome with:\n--whitelisted-extension-id=" + chrome.runtime.id);
+            console.log('Please run chrome with: --whitelisted-extension-id=' + chrome.runtime.id);
             return;
         }
         var tabCaptureStream = new MediaStream();
         try {
             stream.getTracks().forEach(function (track) {
+                console.log('Adding track from tabCapture to MediaStream: ', track)
                 tabCaptureStream.addTrack(track);
             });
         } catch (e) {
+            console.log('Error while reading tabCapture streams')
+            console.log(e)
             return;
         }
 
@@ -102,27 +93,44 @@ function startScreenRecording() {
         }
 
         var mediaRecorder = new MediaRecorder(stream, options);
+        var sequenceID = 0;
         mediaRecorder.ondataavailable = function (event) {
             if (event.data.size > 0) {
+                sequenceID++;
+                console.log(`Sending chunk ${sequenceID} to API.`)
                 var xhr = new XMLHttpRequest();
                 xhr.open('POST', apiURL, true);
                 xhr.setRequestHeader('Content-Type', "video/webm");
                 xhr.setRequestHeader('X-ID', "test.webm")
+                xhr.setRequestHeader('X-Sequence-ID', sequenceID)
                 xhr.send(event.data);
-                xhr.onreadystatechange = function () { // Call a function when the state changes.
+                xhr.onreadystatechange = function () {
                     if (this.readyState === XMLHttpRequest.DONE) {
                         if (this.status === 200) {
-                            console.log("Succesfuly send chunk to API.");
+                            console.log(`Succesfuly send chunk ${sequenceID} to API.`);
                             return
                         }
-                        console.log("Error while sending chunk to API");
+                        console.log(`Error while sending chunk ${sequenceID} to API.`);
                         console.log(this);
                     }
                 }
             }
+        };
+        mediaRecorder.onerror = function (event) {
+            console.log('Media recorder error.')
+            console.log(event);
+            isRecording = false;
+            mediaRecorder.stop();
+        };
+        mediaRecorder.onstop = function (event) {
+            console.log('Media recorder stop.')
+            console.log(event);
+            isRecording = false;
         };
 
         mediaRecorder.start(5000);
         isRecording = true;
     });
 };
+
+console.log('iw-conference-recorder-extension loaded')
